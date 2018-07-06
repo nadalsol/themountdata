d34e8a5eb6fc9b2c834799892806db036d10bbcb
Nadal Soler <nadalsoler@gmail.com>
2018-07-06 12:07:52 +0200

    feat: modal adjustments

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -7,121 +7,127 @@
 
 $(document).ready(function() {
 
-  var header = $('#header'),
-      headerHamburger = $('#header-hamburger'),
-      headerNav = $('#header-nav'),
-      navMainServices = $('#nav-main-services'),
-      navSecondaryServices = $('#nav-secondary-services'),
-      navMainSolutions = $('#nav-main-solutions'),
-      navSecondarySolutions = $('#nav-secondary-solutions'),
-      navMainLangCa = $('#nav-main-lang-ca'),
-      navMainLangEn = $('#nav-main-lang-en'),
-      navMainLangEs = $('#nav-main-lang-es'),
-      navSecondaryLangCa = $('#nav-secondary-lang-ca'),
-      navSecondaryLangEn = $('#nav-secondary-lang-en'),
-      navSecondaryLangEs = $('#nav-secondary-lang-es');
-
   /**
-   * Manage `is-sticky` state for `header`
+   * Navigation menus
    */
-  // var toggleIsSticky = function() {
-  //   var scrollTop = $(window).scrollTop();
-  //   if (scrollTop > 0) {
-  //     header.addClass('is-sticky');
-  //   } else {
-  //     header.removeClass('is-sticky');
-  //   }
-  // };
-  //
-  // toggleIsSticky();
-  //
-  // $(window).scroll(function() {
-  //   toggleIsSticky();
-  // });
+  (function() {
+    var header = $('#header'),
+        headerHamburger = $('#header-hamburger'),
+        headerNav = $('#header-nav'),
+        navMainServices = $('#nav-main-services'),
+        navSecondaryServices = $('#nav-secondary-services'),
+        navMainSolutions = $('#nav-main-solutions'),
+        navSecondarySolutions = $('#nav-secondary-solutions'),
+        navMainLangCa = $('#nav-main-lang-ca'),
+        navMainLangEn = $('#nav-main-lang-en'),
+        navMainLangEs = $('#nav-main-lang-es'),
+        navSecondaryLangCa = $('#nav-secondary-lang-ca'),
+        navSecondaryLangEn = $('#nav-secondary-lang-en'),
+        navSecondaryLangEs = $('#nav-secondary-lang-es');
 
-  /**
-   * Toggle `nav-secondary-services` visibility, via nav-main menu
-   */
-  navMainServices.click(function(event) {
-    event.preventDefault();
+    //
+    // Manage `is-sticky` state for `header`
+    //
+    // var toggleIsSticky = function() {
+    //   var scrollTop = $(window).scrollTop();
+    //   if (scrollTop > 0) {
+    //     header.addClass('is-sticky');
+    //   } else {
+    //     header.removeClass('is-sticky');
+    //   }
+    // };
+    //
+    // toggleIsSticky();
+    //
+    // $(window).scroll(function() {
+    //   toggleIsSticky();
+    // });
 
-    navMainServices.addClass('is-active');
-    navSecondaryServices.toggleClass('is-medium-hidden');
+    //
+    // Toggle `nav-secondary-services` visibility, via nav-main menu
+    //
+    navMainServices.click(function(event) {
+      event.preventDefault();
 
-    navMainSolutions.removeClass('is-active');
-    navSecondarySolutions.addClass('is-medium-hidden');
+      navMainServices.addClass('is-active');
+      navSecondaryServices.toggleClass('is-medium-hidden');
 
-    navSecondaryLangCa.addClass('is-medium-hidden');
-    navSecondaryLangEn.addClass('is-medium-hidden');
-    navSecondaryLangEs.addClass('is-medium-hidden');
-  });
+      navMainSolutions.removeClass('is-active');
+      navSecondarySolutions.addClass('is-medium-hidden');
 
-  /**
-   * Toggle `nav-secondary-solutions` visibility, via nav-main menu
-   */
-  navMainSolutions.click(function(event) {
-    event.preventDefault();
+      navSecondaryLangCa.addClass('is-medium-hidden');
+      navSecondaryLangEn.addClass('is-medium-hidden');
+      navSecondaryLangEs.addClass('is-medium-hidden');
+    });
 
-    navMainServices.removeClass('is-active');
-    navSecondaryServices.addClass('is-medium-hidden');
+    //
+    // Toggle `nav-secondary-solutions` visibility, via nav-main menu
+    //
+    navMainSolutions.click(function(event) {
+      event.preventDefault();
 
-    navMainSolutions.addClass('is-active');
-    navSecondarySolutions.toggleClass('is-medium-hidden');
+      navMainServices.removeClass('is-active');
+      navSecondaryServices.addClass('is-medium-hidden');
 
-    navSecondaryLangCa.addClass('is-medium-hidden');
-    navSecondaryLangEn.addClass('is-medium-hidden');
-    navSecondaryLangEs.addClass('is-medium-hidden');
-  });
+      navMainSolutions.addClass('is-active');
+      navSecondarySolutions.toggleClass('is-medium-hidden');
 
-  /**
-   * Toggle `nav-secondary-lang-ca` visibility, via nav-lang menu
-   */
-  navMainLangCa.click(function(event) {
-    event.preventDefault();
+      navSecondaryLangCa.addClass('is-medium-hidden');
+      navSecondaryLangEn.addClass('is-medium-hidden');
+      navSecondaryLangEs.addClass('is-medium-hidden');
+    });
 
-    navSecondaryLangCa.toggleClass('is-medium-hidden');
-    navSecondaryLangEn.addClass('is-medium-hidden');
-    navSecondaryLangEs.addClass('is-medium-hidden');
+    //
+    // Toggle `nav-secondary-lang-ca` visibility, via nav-lang menu
+    //
+    navMainLangCa.click(function(event) {
+      event.preventDefault();
 
-    navSecondaryServices.addClass('is-medium-hidden');
-    navSecondarySolutions.addClass('is-medium-hidden');
-  });
+      navSecondaryLangCa.toggleClass('is-medium-hidden');
+      navSecondaryLangEn.addClass('is-medium-hidden');
+      navSecondaryLangEs.addClass('is-medium-hidden');
 
-  /**
-   * Toggle `nav-secondary-lang-en` visibility, via nav-lang menu
-   */
-  navMainLangEn.click(function(event) {
-    event.preventDefault();
+      navSecondaryServices.addClass('is-medium-hidden');
+      navSecondarySolutions.addClass('is-medium-hidden');
+    });
 
-    navSecondaryLangCa.addClass('is-medium-hidden');
-    navSecondaryLangEn.toggleClass('is-medium-hidden');
-    navSecondaryLangEs.addClass('is-medium-hidden');
+    //
+    // Toggle `nav-secondary-lang-en` visibility, via nav-lang menu
+    //
+    navMainLangEn.click(function(event) {
+      event.preventDefault();
 
-    navSecondaryServices.addClass('is-medium-hidden');
-    navSecondarySolutions.addClass('is-medium-hidden');
-  });
+      navSecondaryLangCa.addClass('is-medium-hidden');
+      navSecondaryLangEn.toggleClass('is-medium-hidden');
+      navSecondaryLangEs.addClass('is-medium-hidden');
 
-  /**
-   * Toggle `nav-secondary-lang-es` visibility, via nav-lang menu
-   */
-  navMainLangEs.click(function(event) {
-    event.preventDefault();
+      navSecondaryServices.addClass('is-medium-hidden');
+      navSecondarySolutions.addClass('is-medium-hidden');
+    });
 
-    navSecondaryLangCa.addClass('is-medium-hidden');
-    navSecondaryLangEn.addClass('is-medium-hidden');
-    navSecondaryLangEs.toggleClass('is-medium-hidden');
+    //
+    // Toggle `nav-secondary-lang-es` visibility, via nav-lang menu
+    //
+    navMainLangEs.click(function(event) {
+      event.preventDefault();
 
-    navSecondaryServices.addClass('is-medium-hidden');
-    navSecondarySolutions.addClass('is-medium-hidden');
-  });
+      navSecondaryLangCa.addClass('is-medium-hidden');
+      navSecondaryLangEn.addClass('is-medium-hidden');
+      navSecondaryLangEs.toggleClass('is-medium-hidden');
 
-  /**
-   * Toggle `header-nav` visibility, via hamburger menu
-   */
-  headerHamburger.click(function(event) {
-    event.preventDefault();
-    headerNav.toggleClass('is-small-hidden');
-  });
+      navSecondaryServices.addClass('is-medium-hidden');
+      navSecondarySolutions.addClass('is-medium-hidden');
+    });
+
+    //
+    // Toggle `header-nav` visibility, via hamburger menu
+    //
+    headerHamburger.click(function(event) {
+      event.preventDefault();
+      headerNav.toggleClass('is-small-hidden');
+    });
+  })();
+
 
   /**
    * Cookie message
@@ -188,105 +194,114 @@ $(document).ready(function() {
       cookiePath = "/";
     }
     createCookie('seen-cookie-message','yes',cookieExpiry,cookiePath);
+
+    //
+    // Message visibility
+    //
+    // 1. Remove body class.
+    // 2. Remove message from DOM.
+    //
+    var body = $('body'),
+        cookieMessage = $('#cookie-message'),
+        cookieMessageClose = $('#cookie-message-close');
+
+    cookieMessageClose.click(function() {
+      body.removeClass('is-cookie-message'); // 1
+      cookieMessage.remove(); // 2
+    });
   })();
 
-  /**
-   * Close cookie message
-   */
-  var body = $('body'),
-      cookieMessage = $('#cookie-message'),
-      cookieMessageClose = $('#cookie-message-close');
-
-  cookieMessageClose.click(function() {
-    // Remove body class
-    body.removeClass('is-cookie-message');
-    // Remove message from DOM
-    cookieMessage.remove();
-  });
 
   /**
    * Modal
    */
-  var body = $('body'),
+  (function() {
+    var body = $('body'),
+        modalTriggerCookieMessage = $('#modal-trigger-cookie-message'),
+        modalCookieMessage = $('#modal-cookie'),
+        modalCloseCookieMessage = modalCookieMessage.find('.js-modal-close'),
+        modalTriggerPrivacy = $('#modal-trigger-privacy'),
+        modalPrivacy = $('#modal-privacy'),
+        modalClosePrivacy = modalPrivacy.find('.js-modal-close'),
+        modalTriggerCookie = $('#modal-trigger-cookie'),
+        modalCookie = $('#modal-cookie'),
+        modalCloseCookie = modalCookie.find('.js-modal-close'),
+        modalTriggerTerms = $('#modal-trigger-terms'),
+        modalTerms = $('#modal-terms'),
+        modalCloseTerms = modalTerms.find('.js-modal-close');
 
-      modalTriggerCookieMessage = $('#modal-trigger-cookie-message'),
-      modalCookieMessage = $('#modal-cookie'),
-      modalCloseCookieMessage = modalCookieMessage.find('.js-modal-close'),
+    //
+    // Modal open
+    //
+    // 1. Body scroll is removed.
+    // 2. Modal is visible.
+    //
+    modalTriggerCookieMessage.click(function(event) {
+      event.preventDefault();
+      body.addClass('is-modal-open'); // 1
+      modalCookieMessage.removeClass('is-hidden'); // 2
+    });
 
-      modalTriggerPrivacy = $('#modal-trigger-privacy'),
-      modalPrivacy = $('#modal-privacy'),
-      modalClosePrivacy = modalPrivacy.find('.js-modal-close'),
+    modalTriggerPrivacy.click(function(event) {
+      event.preventDefault();
+      body.addClass('is-modal-open'); // 1
+      modalPrivacy.removeClass('is-hidden'); // 2
+    });
 
-      modalTriggerCookie = $('#modal-trigger-cookie'),
-      modalCookie = $('#modal-cookie'),
-      modalCloseCookie = modalCookie.find('.js-modal-close'),
+    modalTriggerCookie.click(function(event) {
+      event.preventDefault();
+      body.addClass('is-modal-open'); // 1
+      modalCookie.removeClass('is-hidden'); // 2
+    });
 
-      modalTriggerTerms = $('#modal-trigger-terms'),
-      modalTerms = $('#modal-terms'),
-      modalCloseTerms = modalTerms.find('.js-modal-close');
+    modalTriggerTerms.click(function(event) {
+      event.preventDefault();
+      body.addClass('is-modal-open'); // 1
+      modalTerms.removeClass('is-hidden'); // 2
+    });
 
-  //
-  // Modal open
-  //
-  // 1. Body scroll is removed.
-  // 2. Modal is visible.
-  //
-  modalTriggerCookieMessage.click(function(event) {
-    event.preventDefault();
-    body.addClass('is-modal-open'); // 1
-    modalCookieMessage.removeClass('is-hidden'); // 2
-  });
+    //
+    // Modal close
+    //
+    // 1. Body scroll is back.
+    // 2. Modal is hidden.
+    //
+    modalCloseCookieMessage.click(function() {
+      body.removeClass('is-modal-open'); // 1
+      modalCookieMessage.addClass('is-hidden'); // 2
+    });
 
-  modalTriggerPrivacy.click(function(event) {
-    event.preventDefault();
-    body.addClass('is-modal-open'); // 1
-    modalPrivacy.removeClass('is-hidden'); // 2
-  });
+    modalClosePrivacy.click(function() {
+      body.removeClass('is-modal-open'); // 1
+      modalPrivacy.addClass('is-hidden'); // 2
+    });
 
-  modalTriggerCookie.click(function(event) {
-    event.preventDefault();
-    body.addClass('is-modal-open'); // 1
-    modalCookie.removeClass('is-hidden'); // 2
-  });
+    modalCloseCookie.click(function() {
+      body.removeClass('is-modal-open'); // 1
+      modalCookie.addClass('is-hidden'); // 2
+    });
 
-  modalTriggerTerms.click(function(event) {
-    event.preventDefault();
-    body.addClass('is-modal-open'); // 1
-    modalTerms.removeClass('is-hidden'); // 2
-  });
+    modalCloseTerms.click(function() {
+      body.removeClass('is-modal-open'); // 1
+      modalTerms.addClass('is-hidden'); // 2
+    });
 
-  //
-  // Modal close
-  //
-  // 1. Body scroll is back.
-  // 2. Modal is hidden.
-  //
-  modalCloseCookieMessage.click(function() {
-    body.removeClass('is-modal-open'); // 1
-    modalCookieMessage.addClass('is-hidden'); // 2
-  });
-
-  modalClosePrivacy.click(function() {
-    body.removeClass('is-modal-open'); // 1
-    modalPrivacy.addClass('is-hidden'); // 2
-  });
-
-  modalCloseCookie.click(function() {
-    body.removeClass('is-modal-open'); // 1
-    modalCookie.addClass('is-hidden'); // 2
-  });
-
-  modalCloseTerms.click(function() {
-    body.removeClass('is-modal-open'); // 1
-    modalTerms.addClass('is-hidden'); // 2
-  });
-
-  //
-  // Modal close (ESC key)
-  //
-  // 1. Body scroll is back, modal is hidden.
-  //
-  // pending...
+    //
+    // Modal close (ESC key)
+    //
+    // 1. Body scroll is back.
+    // 2. Modal is hidden.
+    //
+    $(document).keydown(function(event) {
+      if (event.keyCode == 27 && body.hasClass('is-modal-open')) {
+        body.removeClass('is-modal-open'); // 1
+        modalCookieMessage.addClass('is-hidden'); // 2
+        modalPrivacy.addClass('is-hidden'); // 2
+        modalCookie.addClass('is-hidden'); // 2
+        modalTerms.addClass('is-hidden'); // 2
+      }
+    });
+  })();
 
 });
 
